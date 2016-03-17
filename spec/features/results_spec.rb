3e50d6528f38ab710e882289d0f4906499bfb073
Anders Fisher <anders@devmountain.co.uk>
2016-03-17 14:55:19 +0000

    Add more demanding live update feature

--- spec/features/results_spec.rb
+++ spec/features/results_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 describe 'Visiting the results page', javascript: true do
-  it 'has a set fo results visible' do
+  it 'has a set of results visible' do
     create(:result)
     visit results_path
+    expect(page).to have_selector ".results .result"
+  end
+
+  xit 'shows a new result when it is created' do
+    visit results_path
+    create(:result)
     expect(page).to have_selector ".results .result"
   end
 end
